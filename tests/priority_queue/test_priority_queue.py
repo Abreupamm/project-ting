455f1b249538a33e07dc1aaeaae5fd7d707f3f37
Pamela <abrepamm@gmail.com>
2023-03-07 19:48:26 -0300

    feat: Implementa os testes para a classe PriorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,51 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+files_list = [
+    {
+            'nome_do_arquivo': 'teste1.txt',
+            'qtd_linhas': 4,
+            'linhas_do_arquivo': [
+                'linha',
+                'linha',
+                'linha',
+                'linha',
+            ],
+    },
+    {
+            'nome_do_arquivo': 'teste1.txt',
+            'qtd_linhas': 2,
+            'linhas_do_arquivo': [
+                'linha',
+                'linha',
+            ],
+    },
+    {
+            'nome_do_arquivo': 'teste1.txt',
+            'qtd_linhas': 5,
+            'linhas_do_arquivo': [
+                'linha',
+                'linha',
+                'linha',
+                'linha',
+                'linha',
+            ],
+    },
+]
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority = PriorityQueue()
+
+    for item in files_list:
+        priority.enqueue(item)
+
+    with pytest.raises(IndexError):
+        priority.search(5)
+
+    assert len(priority.regular_priority) == 1
+    assert len(priority.high_priority) == 2
+    assert priority.is_priority(files_list[0]) is True
+    assert priority.is_priority(files_list[2]) is False
+    assert priority.search(2) == files_list[2]
+    assert priority.dequeue() == files_list[0]
