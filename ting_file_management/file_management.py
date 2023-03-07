500c42710c5911335c7937682a1321c8a3ce2c18
Pamela <abrepamm@gmail.com>
2023-03-07 16:18:51 -0300

    feat:  Implementa a função txt_importer, capaz de importar notícias a partir de um arquivo TXT

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if '.txt' not in path_file:
+        return sys.stderr.write('Formato inválido')
+    try:
+        with open(path_file, 'r') as file:
+            result = file.read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        return result
