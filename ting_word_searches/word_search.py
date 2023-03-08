6f9ae8925c16d78249c340c9033b8bb078cbb408
Pamela <abrepamm@gmail.com>
2023-03-08 10:10:43 -0300

    feat: Implementa a função search_by_word, que busca uma palavra em todos os arquivos processados.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,20 +1,35 @@
-def exists_word(word, instance):
-    result = []
+def search_ocorrencias(word, instance, content):
     occurrences = []
     for item in range(instance.__len__()):
         file = instance.search(item)
         for index, value in enumerate(file['linhas_do_arquivo']):
             if word.lower() in value.lower():
-                occurrences.append({'linha': index + 1})
+                if content is True:
+                    occurrences.append({'conteudo': value, 'linha': index + 1})
+                else:
+                    occurrences.append({'linha': index + 1})
+    return {'occurrences': occurrences, 'file': file}
 
-    if len(occurrences) > 0:
+
+def exists_word(word, instance):   
+    result = []
+    search = search_ocorrencias(word, instance, False)
+    if len(search['occurrences']) > 0:
         result.append({
             'palavra': word,
-            'arquivo': file['nome_do_arquivo'],
-            'ocorrencias': occurrences,
+            'arquivo': search['file']['nome_do_arquivo'],
+            'ocorrencias': search['occurrences'],
         })
     return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    search = search_ocorrencias(word, instance, True)
+    if len(search['occurrences']) > 0:
+        result.append({
+            'palavra': word,
+            'arquivo': search['file']['nome_do_arquivo'],
+            'ocorrencias': search['occurrences']
+        })
+    return result
